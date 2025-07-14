3e62b2bfc2688221271b604c1e39de9ee3e27018
Fankhauser-Dominik <domi.fankhauser@hotmail.com>
2025-07-14 11:45:31 +0200

    chokidar instead of chokidar-cli

--- watcher.js
+++ watcher.js
@@ -1,7 +1,7 @@
+const chokidar = require("chokidar");
 const { exec } = require("child_process");
-const path = process.argv[2];
 
-const run = (cmd) =>
+const run = cmd =>
   new Promise((resolve, reject) => {
     exec(cmd, (err, stdout, stderr) => {
       if (stdout) process.stdout.write(stdout);
@@ -11,11 +11,10 @@ const run = (cmd) =>
     });
   });
 
-(async () => {
+const onChange = async path => {
   try {
     if (path) {
-      console.log("PATH: ", path)
-      if (path.includes("admin-base/materialize") || path.includes("admin-base\\materialize")) {
+      if (path.includes("admin-base/materialize")) {
         await run("npm run build:css:patch");
         console.log("Materialize CSS updated");
       } else if (path.includes("admin-base")) {
@@ -24,8 +23,24 @@ const run = (cmd) =>
       }
     }
 
-    console.log("\x1b[32m%s\x1b[0m", "✅ Everything up to date! Waiting for new changes...");
+    console.log(
+      "\x1b[32m%s\x1b[0m",
+      "✅ Everything up to date! Waiting for new changes..."
+    );
   } catch (err) {
     console.error("\x1b[31m%s\x1b[0m", `❌ Build failed: ${err.message}`);
   }
-})();
+};
+
+const watcher = chokidar.watch("admin-base/", {
+  ignored: path =>
+    path.includes("admin-base/ui.apps/target") ||
+    path.includes("admin-base/materialize/target"),
+  ignoreInitial: false
+});
+console.log(
+  "\x1b[34m%s\x1b[0m",
+  "🔭 Watcher is running. Waiting for changes..."
+);
+
+watcher.on("change", onChange);
