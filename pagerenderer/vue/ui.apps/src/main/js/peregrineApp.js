30fd867e82398aab8ca978ebe0e91b46bcd197d0
Patrick Zimmermann <patrickmzimmermann@gmail.com>
2025-09-29 13:19:51 +0200

    no optional chaining

--- pagerenderer/vue/ui.apps/src/main/js/peregrineApp.js
+++ pagerenderer/vue/ui.apps/src/main/js/peregrineApp.js
@@ -256,7 +256,7 @@ function processLoadedContent(data, path, firstTime, fromPopState) {
           updateMetaName("robots", getPerView().page.metaRobots);
           updateOpenGraph();
 
-          var newLocation = window?.hiddenPaths?.includes(path) ? window.location.pathname : path;
+          var newLocation = (window.hiddenPaths && typeof window.hiddenPaths.includes === 'function' && window.hiddenPaths.includes(path)) ? window.location.pathname : path;
           if (peregrineApp.isPublicFacingSite() || peregrineApp.isBetaSite()) {
             newLocation = newLocation.replace($peregrineSiteRoot, "");
           }
