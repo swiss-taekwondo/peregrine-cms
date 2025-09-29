579406647286c17df1a84dd77c1bb2f7da6d86ef
Patrick Zimmermann <patrickmzimmermann@gmail.com>
2025-09-29 13:07:48 +0200

    support hidden paths: display content without updating url path

--- pagerenderer/vue/ui.apps/src/main/js/peregrineApp.js
+++ pagerenderer/vue/ui.apps/src/main/js/peregrineApp.js
@@ -256,7 +256,7 @@ function processLoadedContent(data, path, firstTime, fromPopState) {
           updateMetaName("robots", getPerView().page.metaRobots);
           updateOpenGraph();
 
-          var newLocation = path;
+          var newLocation = window?.hiddenPaths?.includes(path) ? window.location.pathname : path;
           if (peregrineApp.isPublicFacingSite() || peregrineApp.isBetaSite()) {
             newLocation = newLocation.replace($peregrineSiteRoot, "");
           }
