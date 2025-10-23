bb0187e4ea2cbfd7bfd44c030eeb17a7832c386a
Patrick Zimmermann <patrickmzimmermann@gmail.com>
2025-10-23 15:55:59 +0200

    clear button for dates, fixed richtext being editable when form is readonly

--- admin-base/ui.apps/src/main/content/jcr_root/apps/field/material-datepicker/template.vue
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/field/material-datepicker/template.vue
@@ -1,19 +1,42 @@
 <template>
-		<datetime v-model="sanitizedValue" type="date" placeholder="Select date"></datetime>
+	<div class="datetime-input-wrapper">
+		<datetime
+      v-model="sanitizedValue"
+        type="date"
+        placeholder="Select date"
+      />
+		<button
+			:disabled="!value || schema.readonly"
+			@click="clearValue"
+			type="button"
+			class="clear-button"
+			title="Clear date"
+		>
+			<i class="material-icons">clear</i>
+		</button>
+	</div>
 </template>
 
-<script>	
+<script>
 	export default {
 		mixins: [ VueFormGenerator.abstractField ],
-	  computed: {
+			computed: {
 			sanitizedValue: {
 				get () {
-      		return this.value ? this.value : ''
+					return this.value ? this.value : ''
 				},
 				set (newValue) {
 					this.value = newValue
 				}
 			}
-		}
+		},
+		methods: {
+			clearValue() {
+        this.value = "";
+			}
+		},
+    updated() {
+      console.log('updated', this, this.readOnly)
+    }
 	}
 </script>
