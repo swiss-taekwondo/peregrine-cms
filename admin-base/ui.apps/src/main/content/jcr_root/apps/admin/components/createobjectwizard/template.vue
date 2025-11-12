f7bcd0f8038bb03ec1cd13a2df5d8ae78a9f6770
Patrick Zimmermann <patrickmzimmermann@gmail.com>
2025-11-12 12:05:10 +0100

    disallowing underscore

--- admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/createobjectwizard/template.vue
+++ admin-base/ui.apps/src/main/content/jcr_root/apps/admin/components/createobjectwizard/template.vue
@@ -175,8 +175,8 @@
                 if(!value || value.length === 0) {
                     return ['name is required']
                 }
-                if(value.match(/[^0-9a-zA-Z_-]/)) {
-                    return ['object names may only contain letters, numbers, underscores, and dashes']
+                if(value.match(/[^0-9a-zA-Z-]/)) {
+                    return ['object names may only contain letters, numbers and dashes. Google dislikes underscores in URLs']
                 }
                 return [];
             },
